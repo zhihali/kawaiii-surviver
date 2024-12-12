22c79de2f73743ecba0506644c313888a4435da2
Fools <rewqppklt@163.com>
2024-12-12 15:27:18 +0000

    make character move by joystick

--- Assets/Kawaii Survivor/Scripts/PlayerController.cs
+++ Assets/Kawaii Survivor/Scripts/PlayerController.cs
@@ -1,19 +1,21 @@
-using UnityEngine;  // Make sure to include this
+using UnityEngine; 
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rig;
+    [SerializeField] private MobileJoystick playerJoystick;
+    [SerializeField] private float moveSpeed;
 
     // called once when the script is first enabled
     void Start(){
-        
+        rig = GetComponent<Rigidbody2D>();
+        rig.linearVelocity = Vector2.right;
     }
 
     // called every frame
     void Update(){
-        rig = GetComponent<Rigidbody2D>();
-        rig.linearVelocity = Vector2.right;
+        rig.linearVelocity = playerJoystick.GetMoveVector() * moveSpeed * Time.deltaTime;
     }
 }
 
