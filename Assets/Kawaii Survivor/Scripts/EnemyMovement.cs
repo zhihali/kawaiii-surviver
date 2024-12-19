0fb426104566f6bf196c170c25ab67a84e19a73b
Fools <rewqppklt@163.com>
2024-12-19 15:59:43 +0000

    enemy touch character, enemy will be destroyed

--- Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -6,8 +6,13 @@ public class EnemyMovement: MonoBehaviour
 {
     [Header("Element")]
     private Player player;
+
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float playerDetectionRadius;
+
+    [Header("Debug")]
+    [SerializeField] private bool gizmos;
 
     void Start()
     {
@@ -21,18 +26,42 @@ public class EnemyMovement: MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-    // Calculate the direction to the player
-    Vector2 direction = (player.transform.position - transform.position).normalized;
-
-    // Check the distance to the player
-    float distanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
-
-    // If close enough, stop moving
-    if (distanceToPlayer > 0.1f) // Adjust threshold as needed
-    {
-        Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
-        transform.position = targetPosition;
+        FollowPlayer();
+        TryAttack();
     }
+
+    private void FollowPlayer(){
+        // Calculate the direction to the player
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+
+        // Check the distance to the player
+        float distanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
+
+        // If close enough, stop moving
+        if (distanceToPlayer > 0.1f) // Adjust threshold as needed
+        {
+            Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
+            transform.position = targetPosition;
+        }
+    }
+
+    private void TryAttack(){
+        // Check the distance to the player
+        float distanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
+
+        // If close enough, stop moving
+        if (distanceToPlayer < playerDetectionRadius) // Adjust threshold as needed
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!gizmos)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 
 }
